c170b3adbeae94c98af725246b3d43f693a57260
Matt Helmer <mahelmer28@gmail.com>
2024-01-16 11:39:49 -0800

    Chapter13 gloss work in progress

---
+++ Chapter13.hs
@@ -0,0 +1,35 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import Graphics.Gloss
+
+displayMode :: Display
+displayMode = InWindow "Axes" (1000, 700) (10, 10)
+
+axes :: Picture
+axes = Pictures [Color red $ Line [(0, 0), (100, 0)]
+                ,Color green $ Line [(0, 0), (0, 100)]
+                ]
+
+blueCircle :: Picture
+blueCircle = Color blue (Circle 100)
+
+disk :: Float -> Picture
+disk radius = ThickCircle (radius / 2) radius
+
+redDisk :: Picture
+redDisk = Color red (disk 100)
+
+wholePicture :: Picture
+wholePicture = Pictures [Translate (-120) 0 blueCircle
+                        ,Translate   120  0 redDisk
+                        ]
+
+main :: IO ()
+main = display displayMode black wholePicture
+
+
+
+
+-------------------
+-- * Exercise 13.1
+-------------------
