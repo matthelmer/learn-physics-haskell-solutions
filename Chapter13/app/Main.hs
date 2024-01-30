cb4fc5f2894ab4f1722c63bcb1a138056a0e522c
Matt Helmer <mahelmer28@gmail.com>
2024-01-30 04:16:40 -0800

    13.1

--- Chapter13/app/Main.hs
+++ Chapter13/app/Main.hs
@@ -2,77 +2,39 @@
 
 module Main where
 
---import Graphics.Gloss
-import Vis
+import Graphics.Gloss
 
 type R = Double
-
-
---displayMode :: Display
---displayMode = InWindow "My Window" (1000, 700) (10, 10)
-
---axes :: Picture
---axes = Pictures [Color red    $ Line [(0, 0), ( 100,   0)]
---                ,Color green  $ Line [(0, 0), (   0, 100)]
---                ,Color blue   $ Line [(10, 10), (  0, 100)]
---                ,Color yellow $ Line [(10, 0), (   0, 100)]
---                ]
-
---blueCircle :: Picture
---blueCircle = Color blue (Circle 100)
-
---disk :: Float -> Picture
---disk radius = ThickCircle (radius / 2) radius
-
---redDisk :: Picture
---redDisk = Color red (disk 25)
-
---projectileMotion :: Float -> Picture
---projectileMotion t = Translate (xDisk t) (yDisk t) redDisk
-
---xDisk :: Float -> Float
---xDisk t = 40 * t
-
---yDisk :: Float -> Float
---yDisk t = 80 * t - 4.9 * t**2
-
---wholePicture :: Picture
---wholePicture = Pictures [Translate (-120) 0 blueCircle
---                        ,Translate   120  0 redDisk
---                        ]
-
--- simulate function
-
--- updates per second of real time
-rate :: Int
-rate = 24
-
-type Position = (Float,Float)
-type Velocity = (Float,Float)
-type State = (Position,Velocity)
-
-initialState :: State
-initialState = ((0,0),(40,80))
-
---displayFunc :: State -> Picture
---displayFunc ((x,y),_) = Translate x y redDisk
-
-updateFunc :: Float -> State -> State
-updateFunc dt ((x,y),(vx,vy))
-    = (( x + vx * dt, y +  vy * dt)
-      ,(vx          ,vy - 9.8 * dt))
-
-blueCube :: VisObject R
-blueCube = Cube 1 Solid blue
-
-main :: IO ()
-main = display defaultOpts blueCube
---main = simulate displayMode black rate initialState displayFunc
---        (\_ -> updateFunc)
-
-
-
 
 -------------------
 -- * Exercise 13.1
 -------------------
+-- Make an interesting picture using the display function.
+--
+
+displayMode :: Display
+displayMode = InWindow "My Window" (1000, 700) (10, 10)
+
+blueCircle :: Picture
+blueCircle = Color blue (Circle 100)
+
+flagRectangle :: Picture
+flagRectangle = rectangleSolid 500 300
+
+whiteFlag :: Picture
+whiteFlag = Color white flagRectangle
+
+disk :: Float -> Picture
+disk radius = ThickCircle (radius / 2) radius
+
+redDisk :: Picture
+redDisk = Color red (disk 100)
+
+wholePicture :: Picture
+wholePicture = Pictures [Translate 0 0 whiteFlag
+                        ,Translate 0 0 redDisk
+                        ]
+
+
+main :: IO ()
+main = display displayMode black wholePicture
